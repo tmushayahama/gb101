ac06d2dd930666ca05c3cc3e711b06ca02f5ae6f
tmushayahama <tmushayahama@gmail.com>
2013-08-23 05:13:40 -0400

    prototyped A leaderboard section

--- protected/views/site/home.php
+++ protected/views/site/home.php
@@ -27,6 +27,17 @@ Yii::app()->clientScript->registerScriptFile(
 <div class="container-fluid">
 	<div id="main-container">
 		<div id="topbar" class="row-fluid">
+			<div id="gb-topbar-name-title" class="span3">
+				<span class="span2">
+					<img href="/profile" src="<?php echo Yii::app()->request->baseUrl; ?>/img/gb_avatar.jpg" alt="">
+				</span>
+				<span class="span10">
+					<p>
+						<a>Tremayne Mushayahama</a><br>
+						<button class="pull-right btn btn-mini"><i class="icon icon-wrench"></i> Edit</button>
+					</p>
+				</span>
+			</div>
 			<div id="tool-toggles">
 				<!-- preview button -->
 				<div class="btn-group pull-right">
@@ -48,10 +59,10 @@ Yii::app()->clientScript->registerScriptFile(
 				</ul>
 			</div>
 		</div>
-	
+
 		<div class="row-fluid">
 			<!-- gb sidebar menu -->
-				<ul id="sidebar-selector">
+			<ul id="sidebar-selector">
 				<li class="active"><a href="#" data-asset-type="terrain"><div class="icon icon-home"></div><br>Home</a></li>
 				<li id="sidebar-items" ><a href="#" data-asset-type="items"><div class="icon icon-profile"></div><br>Profile</a></li>
 				<li id="sidebar-characters"><a href="#" data-asset-type="characters"><div class="icon icon-characters"></div><br>Groups</a></li>
@@ -65,30 +76,7 @@ Yii::app()->clientScript->registerScriptFile(
 			</div>
 			<div id="sidebar-corner"><div class="outer-shading"></div><div class="curve"></div></div>
 
-			<!-- Home Panel quick links -->
 
-			<div id="gb-home-left-sidebar" class="animated" >
-				<div id="gb-home-left-sidebar-title">
-					<span class="span2">
-						<img href="/profile" src="<?php echo Yii::app()->request->baseUrl; ?>/img/gb_avatar.jpg" alt="">
-					</span>
-					<span class="span6">
-						<p><a>Tremayne Mushayahama</a></p>	
-					</span>
-					<span class="span4">
-						<button class="pull-right btn btn-mini"><i class="icon icon-wrench"></i> Edit</button>
-					</span>
-				</div>
-				<div id="gb-home-left-sidebar-content">
-					<ul class="nav nav-stacked">
-						<li><h5><a href="/">Recent Commitments</a></h5></li>
-						<li><h5><a href="#">Goal List</a></h5></li>
-						<li><h5><a href="#">Friends</a></h5></li>
-						<li><h5><a href="#">Groups</a></h5></li>
-						<li><h5><a href="#">More</a></h5></li>
-					</ul> 
-				</div> 
-			</div>
 			<!-- TOOLS PANE -->
 
 			<!-- <div id="tools-panel-container" class="animated">
@@ -157,75 +145,102 @@ Yii::app()->clientScript->registerScriptFile(
 				</ul>
 			</div>
 			<!-- Posts -->
-			<div id="gb-posts-container" class="span6 animated">
-				<div id="gb-post-input"> 
-					<div id="gb-commit-form" class="row rm-row">
-						<textarea id="gb-add-commitment-input" class="span12"rows="2" placeholder="What is your goal?"></textarea>
-						<ul id="gb-post-tab" class="nav row inline ">
-							<li class="active span4">
-								<a href="#rm-home-add-commitment">
-									<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icons/add_goal.png" class="active" alt=""><br>
-									<strong>Add Goal</strong>
-								</a>
-							</li>
-							<li class="span4">
-								<a href="#rm-home-add-commitment">
-									<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icons/assign_goal.png" 
-											 onmouseover="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/assign_goal_hover.png'" 
-											 onmouseout="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/assign_goal.png'" alt=""><br>
-									<strong>Assign Goal</strong>
-								</a>
-							</li>
-							<li class="span4">
-								<a href="#rm-home-add-commitment">
-									<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icons/goal_challenge.png" 
-											 onmouseover="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/goal_challenge_hover.png'" 
-											 onmouseout="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/goal_challenge.png'" alt=""><br>
-									<strong>Goal Challenge</strong>
-								</a>
-							</li>
-						</ul>
-						<ul class="nav hidden">
-							<li class="pull-right">
-								<button type="submit" id="rm-commit-post-home" class="rm-dark-blue-btn">I Commit</button>
-							</li>
-							<li class="pull-right dropdown">
-								<a href="#" class="dropdown-toggle btn" data-toggle="dropdown">Friends <b class="caret"></b></a>
-								<ul class="dropdown-menu">
-									<li class="nav-header">Who can see this</li>
-									<li id="rm-friends-selector-home" class="controls">
-										<label class="checkbox text-left">
-											<input type="checkbox" value="option1"> Select All
-										</label>
-									</li>
-								</ul>
-							</li>
-							<li class="pull-right">
-								<ul class="inline">
-								</ul>
-							</li>
-						</ul>
+			<div id="gb-home-middle-container" class="span8">
+				<div id="gb-posts-container" class="span7 animated">
+					<div id="gb-post-input"> 
+						<div id="gb-commit-form" class="row rm-row">
+							<textarea id="gb-add-commitment-input" class="span12"rows="2" placeholder="What is your goal?"></textarea>
+							<ul id="gb-post-tab" class="nav row inline ">
+								<li class="active span4">
+									<a href="#rm-home-add-commitment">
+										<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icons/add_goal.png" class="active" alt=""><br>
+										<strong>Add Goal</strong>
+									</a>
+								</li>
+								<li class="span4">
+									<a href="#rm-home-add-commitment">
+										<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icons/assign_goal.png" 
+												 onmouseover="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/assign_goal_hover.png'" 
+												 onmouseout="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/assign_goal.png'" alt=""><br>
+										<strong>Assign Goal</strong>
+									</a>
+								</li>
+								<li class="span4">
+									<a href="#rm-home-add-commitment">
+										<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icons/goal_challenge.png" 
+												 onmouseover="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/goal_challenge_hover.png'" 
+												 onmouseout="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/goal_challenge.png'" alt=""><br>
+										<strong>Goal Challenge</strong>
+									</a>
+								</li>
+							</ul>
+							<ul class="nav hidden">
+								<li class="pull-right">
+									<button type="submit" id="rm-commit-post-home" class="rm-dark-blue-btn">I Commit</button>
+								</li>
+								<li class="pull-right dropdown">
+									<a href="#" class="dropdown-toggle btn" data-toggle="dropdown">Friends <b class="caret"></b></a>
+									<ul class="dropdown-menu">
+										<li class="nav-header">Who can see this</li>
+										<li id="rm-friends-selector-home" class="controls">
+											<label class="checkbox text-left">
+												<input type="checkbox" value="option1"> Select All
+											</label>
+										</li>
+									</ul>
+								</li>
+								<li class="pull-right">
+									<ul class="inline">
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<!-- <ul class="nav rm-nav nav-pills inline span12 rm-green-border">
+						<li class="span6"><a href="#rm-assign-goal-modal" role="button" data-toggle="modal"><h4>Assign a goal</h4></a></li>
+						<li class="span6"><a href="#"><h4>Assign a small task</h4></a></li>
+					</ul>
+					<ul class="nav rm-nav nav-pills inline span12 rm-green-border">
+						<li class="span6"><a href="#"><h4>Create a challenge</h4></a></li>
+						<li class="span6"><a href="#"><h4>Join a challenge</h4></a></li>
+					</ul> -->
+
+					<div id="goal-posts"class="row rm-row rm-container">
+						<?php foreach ($posts as $post): ?>
+							<?php
+							echo $this->renderPartial('_goal_commitment_post', array(
+									"title" => $post->type->type,
+									"description" => $post->description,
+									"points_pledged" => $post->points_pledged
+							));
+							?>
+						<?php endforeach; ?>
 					</div>
 				</div>
-				<!-- <ul class="nav rm-nav nav-pills inline span12 rm-green-border">
-					<li class="span6"><a href="#rm-assign-goal-modal" role="button" data-toggle="modal"><h4>Assign a goal</h4></a></li>
-					<li class="span6"><a href="#"><h4>Assign a small task</h4></a></li>
-				</ul>
-				<ul class="nav rm-nav nav-pills inline span12 rm-green-border">
-					<li class="span6"><a href="#"><h4>Create a challenge</h4></a></li>
-					<li class="span6"><a href="#"><h4>Join a challenge</h4></a></li>
-				</ul> -->
+				<div class="span5">
+					<div id="gb-leaderboard" class="span12">
+						<h4>Leader Board</h4>
+						<table class="table table-condensed">
+							<thead>
+								<tr>
+									<th></th>
+									<th>Today</th>
+									<th>Total</th>
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<th>1</th>
+									<th></th>
+									<th></th>
+									<th></th>
+								</tr>
+							
+							</tbody>
+						</table>
 
-				<div id="goal-posts"class="row rm-row rm-container">
-					<?php foreach ($posts as $post): ?>
-						<?php
-						echo $this->renderPartial('_goal_commitment_post', array(
-								"title"=>$post->type->type,
-								"description"=>$post->description,
-								"points_pledged"=>$post->points_pledged
-								));
-						?>
-					<?php endforeach; ?>
+					</div>
 				</div>
 			</div>
 		</div>
