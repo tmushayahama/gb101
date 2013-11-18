081bce12674aeee6ba35ba1c586aef8d3fd32984
tmushayahama <tmushayahama@gmail.com>
2013-11-18 05:17:37 -0500

    Prototyping the skill list page

--- protected/modules/goal/views/goal/skill_home.php
+++ protected/modules/goal/views/goal/skill_home.php
@@ -139,112 +139,40 @@ Yii::app()->clientScript->registerScriptFile(
               </div>
             </div>
             <div class="tab-pane" id="skill-list-pane">
-              <div class="span12">
-                <div id="gb-goal-skill-list-box" class=" row-fluid">
+              <ul id="gb-skill-activity-nav" class="gb-border-blue-4">
+                <li class=""><a href="#gb-skill-list-all-pane" data-toggle="tab">All<i class="icon-chevron-right pull-right"></i></a></li>
+                <li class="active"><a href="#gb-skill-list-gained-pane" data-toggle="tab">Skills Gained<i class="icon-chevron-right pull-right"></i></a></li>
+                <li class=""><a href="#gb-skill-to-learn-pane" data-toggle="tab">Skills To Learn<i class="icon-chevron-right pull-right"></i></a></li>
+                <li class=""><a href="#gb-skill-to-know-pane" data-toggle="tab">Skills To Know<i class="icon-chevron-right pull-right"></i></a></li>
+                <li class=""><a href="#gb-skill-miscellaneous-pane" data-toggle="tab">Miscellaneous <i class="icon-chevron-right pull-right"></i></a></li>
+                <li class=""><a href="#gb-skill-words-of-action-pane" data-toggle="tab">Words of Action<i class="icon-chevron-right pull-right"></i></a></li>
+              </ul>
+              <div id="gb-skill-activity-content" class="tab-content">
+                <div class="tab-pane active"id="gb-skill-list-gained-pane">
                   <div class="sub-heading-2">
-                    <h3 class="pull-left">Skill List</h3>
+                    <h3 class="pull-left">Skills Gained</h3>
                     <a class="pull-right gb-btn gb-btn-color-white btn-large gb-btn-blue-4 add-skill-modal-trigger" type="1">Add Skills</a>
                   </div>
-                  <div class="row-fluid gb-btn-white-4 gb-shadow-blue-5">
-                    <p> <h5>
-                      Make a list of many skills you are good at or 
-                      skills you want to learn.</h5></p>
-                  </div>
-                  <div id="gb-goal-skill-container" class=" row-fluid">
-                    <?php
-                    $count = 1;
-                    foreach ($skillList as $goalListItem):
-                      echo $this->renderPartial('_skill_list_row_big', array(
-                       'goalListItem' => $goalListItem,
-                       'count' => $count++));
-                    endforeach;
-                    ?>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="tab-pane" id="promise-tab-pane">
-              <div class="span6">
-                <div id="gb-goal-skill-list-box" class=" row span12">
-                  <div class="sub-heading-2">
-                    <h3 class="pull-left">Promise List</h3>
-                    <a class="pull-right gb-btn gb-btn-color-white btn-large gb-btn-blue-1 add-skill-modal-trigger" type="2">Add Promises</a>
-                  </div>
-                  <div id="gb-goal-promise-container" class=" row">
-                    <?php
-                    $count = 1;
-                    foreach ($promiseList as $goalListItem):
-                      echo $this->renderPartial('_skill_list_row', array(
-                       'goalListItem' => $goalListItem,
-                       'count' => $count++));
-                    endforeach;
-                    ?>
-                  </div>
-                </div>
-              </div>
-              <div class="span6">
-                <div id="gb-post-input"> 
-                  <div id="gb-commit-form" class="row rm-row">
-                    <textarea id="gb-add-commitment-input" class="span12"rows="2" placeholder="What is your goal?"></textarea>
-                    <ul id="gb-post-tab" class="nav row inline ">
-                      <li class="active span4">
-                        <a href="#rm-home-add-commitment">
-                          <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icons/add_goal.png" class="active" alt=""><br>
-                          <strong>Add Goal</strong>
-                        </a>
-                      </li>
-                      <li class="span4">
-                        <a href="#rm-home-add-commitment">
-                          <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icons/assign_goal.png" 
-                               onmouseover="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/assign_goal_hover.png'" 
-                               onmouseout="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/assign_goal.png'" alt=""><br>
-                          <strong>Assign Goal</strong>
-                        </a>
-                      </li>
-                      <li class="span4">
-                        <a href="#rm-home-add-commitment">
-                          <img src="<?php echo Yii::app()->request->baseUrl; ?>/img/icons/goal_challenge.png" 
-                               onmouseover="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/goal_challenge_hover.png'" 
-                               onmouseout="this.src = '<?php echo Yii::app()->request->baseUrl; ?>/img/icons/goal_challenge.png'" alt=""><br>
-                          <strong>Goal Challenge</strong>
-                        </a>
-                      </li>
-                    </ul>
-                    <ul class="nav hidden">
-                      <li class="pull-right">
-                        <button type="submit" id="rm-commit-post-home" class="rm-dark-blue-btn">I Commit</button>
-                      </li>
-                      <li class="pull-right dropdown">
-                        <a href="#" class="dropdown-toggle btn" data-toggle="dropdown">Friends <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                          <li class="nav-header">Who can see this</li>
-                          <li id="rm-friends-selector-home" class="controls">
-                            <label class="checkbox text-left">
-                              <input type="checkbox" value="option1"> Select All
-                            </label>
-                          </li>
-                        </ul>
-                      </li>
-                      <li class="pull-right">
-                        <ul class="inline">
-                        </ul>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div id="goal-posts"class="row rm-row rm-container">
-                  <?php foreach ($posts as $post): ?>
-                    <?php
-                    echo $this->renderPartial('_goal_commitment_post', array(
-                     "goalCommitment" => $post->goalCommitment,
-                     'connection_name' => 'All'//$post->connection->name
-                    ));
-                    ?>
-                  <?php endforeach; ?>
-                </div>
-              </div>
-            </div>
+                  <div id="gb-goal-skill-list-box" class=" row-fluid">
 
+                    <div class="row-fluid gb-btn-white-4 gb-shadow-blue-5">
+                      <p> <h5>
+                        Make a list of many skills you have gained so far.</h5></p>
+                    </div>
+                    <div id="gb-goal-skill-container" class=" row-fluid">
+                      <?php
+                      $count = 1;
+                      foreach ($skillList as $goalListItem):
+                        echo $this->renderPartial('_skill_list_row_big', array(
+                         'goalListItem' => $goalListItem,
+                         'count' => $count++));
+                      endforeach;
+                      ?>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
