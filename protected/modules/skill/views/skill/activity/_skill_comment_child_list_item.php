350ae54db28e295a0a0559a02d3a3a839d3426cb
tmushayahama <tmushayahama@gmail.com>
2014-10-19 17:17:17 -0700

    Prettified the commentz tab in skill management

--- protected/modules/skill/views/skill/activity/_skill_comment_child_list_item.php
+++ protected/modules/skill/views/skill/activity/_skill_comment_child_list_item.php
@@ -6,43 +6,41 @@
  */
 ?>
 
-<div class="gb-post-entry gb-bottom-border-grey-1 gb-comment-list-item row gb-padding-left-3" skill-comment-id="<?php echo $skillCommentChild->id; ?>"
+<div class="gb-post-entry gb-comment-list-item row gb-discussion-title-side-border" skill-comment-id="<?php echo $skillCommentChild->id; ?>"
      gb-source-pk-id="<?php echo $skillCommentChild->comment_id; ?>" gb-data-source="<?php echo Type::$SOURCE_TODO; ?>">
 
-  <div class="col-lg-12 col-sm-12 col-xs-12 gb-no-padding gb-margin">
-    <div class="row gb-padding-thin">
+  <div class="col-lg-1 col-md-1 col-sm-1 gb-no-padding">
+    <img src="<?php echo Yii::app()->request->baseUrl . "/img/profile_pic/" . $skillCommentChild->comment->creator->profile->avatar_url; ?>" class="gb-child-box-heading-img img-circle pull-right" alt="">
+  </div>
+  <div class="col-lg-11 col-sm-10 col-xs-12 gb-no-padding gb-no-margin">
+    <div class="row">
       <div class="row gb-panel-form gb-hide">
       </div>
-      <div class="row gb-panel-display">
-        <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 gb-no-padding">
+      <h5 class="gb-child-box-heading">
+        <span> 
+          <a href="<?php echo Yii::app()->createUrl('user/profile/profile/', array('user' => $skillCommentChild->comment->creator_id)); ?>">
+            <?php echo $skillCommentChild->comment->creator->profile->firstname . " " . $skillCommentChild->comment->creator->profile->lastname ?>
+          </a>
+        </span>
+      </h5>
+      <div class="row gb-panel-display gb-padding-left-1">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 gb-no-padding">
           <p><strong class="gb-display-attribute" gb-control-target="#gb-skill-comment-form-title-input"><?php echo $skillCommentChild->comment->title; ?> </strong> 
             <span class="gb-display-attribute" gb-control-target="#gb-skill-comment-form-description-input"><?php echo $skillCommentChild->comment->description; ?></span>
           </p>
         </div>
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 gb-no-padding">
-          <div class="gb-show-more-btn btn btn-link btn-xs pull-right"
-               gb-closest-parent=".gb-comment-list-item">
-            More
-          </div>
-          <div class="btn-group pull-right">
-            <?php if ($skillCommentChild->comment->creator_id == Yii::app()->user->id): ?>
-              <a class="gb-edit-form-show btn btn-xs btn-link"
-                 gb-form-target="#gb-skill-comment-form">
-                <i class="glyphicon glyphicon-edit"></i>
-              </a> 
-              <a class="gb-delete-me btn btn-xs btn-link" gb-del-type="<?php echo Type::$DEL_TYPE_REMOVE; ?>"><i class="glyphicon glyphicon-trash"></i></a>
-            <?php endif; ?>
-          </div> 
-        </div>
-
       </div>
     </div>
-    <div class="gb-hide gb-no-padding gb-show-more">
-      <div class="row gb-padding-left-3">
-        <img src="<?php echo Yii::app()->request->baseUrl . "/img/profile_pic/" . $skillCommentChild->comment->creator->profile->avatar_url; ?>" class="gb-img-sm img-polariod pull-left" alt="">
-        <div class="btn btn-sm pull-left">By: <a href="<?php echo Yii::app()->createUrl('user/profile/profile/', array('user' => $skillCommentChild->comment->creator_id)); ?>"><i><?php echo $skillCommentChild->comment->creator->profile->firstname . " " . $skillCommentChild->comment->creator->profile->lastname ?></i></a></div>
-
-      </div>
+    <div class="row gb-padding-left-1">
+      <div class="btn-group pull-right">
+        <?php if ($skillCommentChild->comment->creator_id == Yii::app()->user->id): ?>
+          <a class="gb-edit-form-show btn btn-xs btn-link"
+             gb-form-target="#gb-skill-comment-form">
+            <i class="glyphicon glyphicon-edit"></i>
+          </a> 
+          <a class="gb-delete-me btn btn-xs btn-link" gb-del-type="<?php echo Type::$DEL_TYPE_REMOVE; ?>"><i class="glyphicon glyphicon-trash"></i></a>
+          <?php endif; ?>
+      </div> 
     </div>
   </div>
 </div> 
--- protected/modules/skill/views/skill/activity/_skill_comment_parent_list_item.php
+++ protected/modules/skill/views/skill/activity/_skill_comment_parent_list_item.php
@@ -5,14 +5,25 @@
  * and open the template in the editor.
  */
 ?>
-<div class="gb-post-entry gb-comment-list-item panel panel-default row gb-background-light-grey-1" skill-comment-id="<?php echo $skillCommentParent->id; ?>"
+<div class="gb-post-entry gb-comment-list-item panel panel-default row gb-discussion-title-side-border" skill-comment-id="<?php echo $skillCommentParent->id; ?>"
      gb-source-pk-id="<?php echo $skillCommentParent->comment_id; ?>" gb-data-source="<?php echo Type::$SOURCE_TODO; ?>">
 
-  <div class="col-lg-12 col-sm-12 col-xs-12 panel panel-default gb-no-padding gb-no-margin gb-discussion-title-side-border">
-    <div class="panel-body gb-padding-thin">
+  <div class="col-lg-1 col-md-1 col-sm-1 gb-no-padding">
+    <img src="<?php echo Yii::app()->request->baseUrl . "/img/profile_pic/" . $skillCommentParent->comment->creator->profile->avatar_url; ?>" class="gb-parent-box-heading-img img-circle pull-right" alt="">
+  </div>
+  <div class="col-lg-11 col-sm-10 col-xs-12 gb-no-padding gb-no-margin">
+    <div class="row">
       <div class="row gb-panel-form gb-hide">
       </div>
-      <div class="row gb-panel-display">
+      <h5 class="gb-parent-box-heading">
+        <span> 
+          <a href="<?php echo Yii::app()->createUrl('user/profile/profile/', array('user' => $skillCommentParent->comment->creator_id)); ?>">
+            <?php echo $skillCommentParent->comment->creator->profile->firstname . " " . $skillCommentParent->comment->creator->profile->lastname ?>
+          </a>
+        </span>
+        <span><i>Skill Comment</i></span>
+      </h5>
+      <div class="row gb-panel-display gb-padding-left-3">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 gb-no-padding">
           <p><strong class="gb-display-attribute" gb-control-target="#gb-skill-comment-form-title-input"><?php echo $skillCommentParent->comment->title; ?> </strong> 
             <span class="gb-display-attribute" gb-control-target="#gb-skill-comment-form-description-input"><?php echo $skillCommentParent->comment->description; ?></span>
@@ -20,43 +31,36 @@
         </div>
       </div>
     </div>
-    <div class="panel-footer gb-no-padding">
-      <div class="row gb-padding-left-1">
-        <img src="<?php echo Yii::app()->request->baseUrl . "/img/profile_pic/" . $skillCommentParent->comment->creator->profile->avatar_url; ?>" class="gb-img-sm img-polariod pull-left" alt="">
-        <div class="btn btn-sm pull-left">By: <a href="<?php echo Yii::app()->createUrl('user/profile/profile/', array('user' => $skillCommentParent->comment->creator_id)); ?>"><i><?php echo $skillCommentParent->comment->creator->profile->firstname . " " . $skillCommentParent->comment->creator->profile->lastname ?></i></a></div>
-        <div class="btn-group pull-right">
-          <a class="btn btn-sm btn-link gb-form-show"
-             gb-is-child-form="1"
-             gb-form-slide-target="<?php echo '#gb-skill-comment-child-form-container-' . $skillCommentParent->id; ?>"
-             gb-form-target="#gb-skill-comment-form"
-             gb-form-parent-id-input="#gb-skill-comment-form-parent-comment-id-input"
-             gb-form-heading="Add Skill Comment"
-             gb-form-parent-id="<?php echo $skillCommentParent->id; ?>"
-             <i class="glyphicon glyphicon-plus"></i>
-            Add a Comment 
-          </a>
-          <?php if ($skillCommentParent->comment->creator_id == Yii::app()->user->id): ?>
-            <a class="gb-edit-form-show btn btn-sm btn-link"
-               gb-form-target="#gb-skill-comment-form">
-              <i class="glyphicon glyphicon-edit"></i>
-            </a> 
-            <a class="gb-delete-me btn btn-sm btn-link" gb-del-type="<?php echo Type::$DEL_TYPE_REMOVE; ?>"><i class="glyphicon glyphicon-trash"></i></a>
-          <?php endif; ?>
-        </div> 
+    <div class="row gb-padding-left-1">
+      <div class="btn-group pull-left">
+        <a class="btn btn-sm btn-link gb-form-show"
+           gb-is-child-form="1"
+           gb-form-slide-target="<?php echo '#gb-skill-comment-child-form-container-' . $skillCommentParent->id; ?>"
+           gb-form-target="#gb-skill-comment-form"
+           gb-form-parent-id-input="#gb-skill-comment-form-parent-comment-id-input"
+           gb-form-heading="Add Skill Comment"
+           gb-form-parent-id="<?php echo $skillCommentParent->id; ?>"
+           <i class="glyphicon glyphicon-plus"></i>
+          Reply
+        </a>        
       </div>
-    </div> 
+      <div class="btn-group pull-right">
+        <?php if ($skillCommentParent->comment->creator_id == Yii::app()->user->id): ?>
+          <a class="gb-edit-form-show btn btn-sm btn-link"
+             gb-form-target="#gb-skill-comment-form">
+            <i class="glyphicon glyphicon-edit"></i>
+          </a> 
+          <a class="gb-delete-me btn btn-sm btn-link" gb-del-type="<?php echo Type::$DEL_TYPE_REMOVE; ?>"><i class="glyphicon glyphicon-trash"></i></a>
+        <?php endif; ?>
+      </div>
+    </div>
     <div id="<?php echo 'gb-skill-comment-child-form-container-' . $skillCommentParent->id; ?>" class="row gb-panel-form gb-hide">
 
     </div>
     <div id="gb-comment-children">
       <?php
       $skillCommentChildren = SkillComment::getSkillChildrenComments($skillCommentParent->id);
-      if (count($skillCommentChildren) == 0):
-        ?>
-        <h5 class="text-center text-warning gb-no-information row">
-          no comment(s) added.
-        </h5>
-      <?php endif; ?>
+      ?>
 
       <?php foreach ($skillCommentChildren as $skillCommentChild): ?>
         <?php
